523700d97ea0def5e53342ae8415b0fe3bc11811
elina2323 <elina.balabaeva2323@gmail.com>
2021-05-01 23:04:47 +0600

    Initial commit

---
+++ src/javalessons/GeoPlugin.java
@@ -0,0 +1,22 @@
+package javalessons;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+public class GeoPlugin  extends DefaultHandler {
+    @Override
+    public void startDocument() throws SAXException {
+        super.startDocument();
+    }
+
+    @Override
+    public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+        System.out.print(qName + ": ");
+    }
+
+    @Override
+    public void characters(char[] ch, int start, int length) throws SAXException {
+        System.out.print(new String(ch,start,length));
+    }
+}
---
+++ src/javalessons/Main.java
@@ -0,0 +1,25 @@
+package javalessons;
+
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+
+public class Main{
+
+
+    public static void main(String[] args) throws ParserConfigurationException, SAXException, URISyntaxException, IOException {
+        SAXParserFactory saxParser = SAXParserFactory.newInstance();
+        SAXParser saxParser1 = saxParser.newSAXParser();
+        GeoPlugin saxParser2 = new GeoPlugin();
+        URL url2 = new URL("http://www.geoplugin.net/xml.gp?base_currency=USD");
+        saxParser1.parse(new InputSource(url2.openStream()),saxParser2);
+
+    }
+}
+
